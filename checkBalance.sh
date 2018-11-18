de1ddde7c8ab4f9372692a33e07a0ae6607b4d96
temp69 <temp@magnetwork.io>
2018-11-18 08:50:36 +0000

    checkBalance

---
+++ checkBalance.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# config:
+# - MASTERNODELIST, add all your masternode addresses in this array
+# e.g.: MASTERNODELIST=('MASTERNODE_ADDRESS_1' 'MASTERNODE_ADDRESS_2' 'MASTERNODE_ADDRESS3');
+# - QUERYURL, the block explorer you can query for a balance of a given address
+# e.g.: QUERYURL='http://blockexplorer.url/ext/getbalance/'
+# - MNCOLLATERAL, masternodes collateral
+# e.g.: MNCOLLATERAL=10000
+# - COINSYMBOL='COUIN';
+# e.g.: COINSYMBOL='MAG'
+
+declare -a MASTERNODELIST=('Mf4PcAzarQygmC23GxULDUrLH5c4WVDWVD' 'Mbtz6WLQmkeoQFw4qyS3AGpMg2LEAvE95N' 'Mu8M7mofaysLxvHZxVXGSXW4LYPWE1avor');
+declare -r QUERYURL='http://209.250.248.159:3001/ext/getbalance/'
+declare -r MNCOLLATERAL=10000;
+declare -r COINSYMBOL='MAG'
+
+COUNT=0;
+REWARD=0;
+for MASTERNODE in "${MASTERNODELIST[@]}"; do
+	BALANCE=$(curl -s --connect-timeout 2 "$QUERYURL$MASTERNODE");
+	EXITSTATUS=$?
+	if [[ "$EXITSTATUS" -eq 0 ]]; then
+		((COUNT++));
+		INT=${BALANCE%.*};
+		REWARD=$((REWARD+INT-$MNCOLLATERAL));
+		echo -e "$MASTERNODE: $BALANCE / $INT / $((INT-$MNCOLLATERAL))";
+	else
+		echo -e "$MASTERNODE: -NA-";
+	fi
+done
+echo -ne "\n$REWARD MAG / $COUNT MASTERNODES = ";
+echo "$((REWARD/COUNT)) $COINSYMBOL";
